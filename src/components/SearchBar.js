cc23a5ba153c547bd932348d5a4cca5f5c561cfd
Jonathan Setzler <jonathan@setzler.de>
2022-04-30 09:48:56 +0200

    make clear x work

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,20 @@
+import { useRef } from "react"
+
 import magnifying_glass from "../images/magnifying_glass.svg"
 import closeX from "../images/closeX.png"
 
 export default function SearchBar(props) {
+    const ref = useRef()
+
+    const clearSearch = () => {
+        ref.current.value= ""
+        props.setSearchText("")
+    }
+
     return <div className="searchBar">
         <img src={magnifying_glass} />
         <form>
-            <input type="text" className="searchBarTextInput" placeholder="search ..." onChange={event => props.setSearchText(event.target.value)} /> </form>
-        <img src={closeX} height={25} width={25}/>
+            <input ref={ref} type="text" className="searchBarTextInput" placeholder="search ..." onChange={event => props.setSearchText(event.target.value)} /> </form>
+        <img src={closeX} height={25} width={25} onClick={clearSearch}/>
     </div>
 }
