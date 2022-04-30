325fb5ebe8b7083c6916321ea854f629cdc045fd
Jonathan Setzler <jonathan@setzler.de>
2022-04-30 09:49:15 +0200

    filter can now be toggled

--- src/views/AppSelectView.js
+++ src/views/AppSelectView.js
@@ -1,4 +1,5 @@
 import filter from "../images/filter.png"
+import clearFilter from "../images/clearFilter.png"
 
 import SearchBar from "../components/SearchBar"
 import AppCard from "../components/AppCard"
@@ -8,10 +9,11 @@ import { useState } from "react"
 export default function AppSelectView(props) {
     const [selectedKraftgruppen, setSelectedKraftgruppen] = useState([])
     const [searchText, setSearchText] = useState("")
-
+    const [filterActive, setFilterActive] = useState(false)
+ 
     const getApps = () => {
-        if (searchText === ""){
-            return props.knownApps.filter(app => selectedKraftgruppen.includes(app.kraftgruppe))
+        if (!filterActive){
+            return props.knownApps.filter(app => app.name.includes(searchText))
         } else {
             return props.knownApps.filter(app => selectedKraftgruppen.includes(app.kraftgruppe) &&
                                         app.name.includes(searchText))
@@ -19,18 +21,50 @@ export default function AppSelectView(props) {
         
     }
 
+    const toggleFilter = () => {
+       setFilterActive(!filterActive)
+       setSelectedKraftgruppen([]) 
+    }
+
+    const filterIcon = () => {
+        return filterActive ? clearFilter : filter
+    }
+
+    const filterOptions = () => {
+        if (filterActive) {
+            return (
+            <div>
+                <KraftgruppenPicker updateSelectedKraftgruppen={setSelectedKraftgruppen}/>   
+            </div>)
+        }
+        
+    }
+
+    const showFilterResults = () => {
+        const apps = getApps()
+        if (apps.length > 0) {
+            return (
+            <div className="appList">
+                {apps.map(app => <AppCard name={app.name} kraftgruppe={app.kraftgruppe} key={app.name}/>)}
+            </div>
+            )
+        } else {
+            return (
+                <div>
+                    <p>keine Apps dieser Kriterien gefunden ...</p>
+                </div>
+            )
+        }
+    }
+
     return (
         <div className="flexcol">
             <div className="flexrow sticky">
                 <SearchBar setSearchText={setSearchText} />
-                <img src={filter} width={35} height={35} />
+                <img src={filterIcon()} width={35} height={35} onClick={toggleFilter} />
             </div>
-            <div>
-                <KraftgruppenPicker updateSelectedKraftgruppen={setSelectedKraftgruppen}/>   
-            </div>
-            <div className="appList">
-                {getApps().map(app => <AppCard name={app.name} kraftgruppe={app.kraftgruppe} key={app.name}/>)}
-            </div>
+            {filterOptions()}
+            {showFilterResults()}
         </div>
     )
 }
