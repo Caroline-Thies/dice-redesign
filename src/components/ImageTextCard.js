a5743408ee3f5d5cdf518d51ac51f17475a50f94
Jonathan Setzler <jonathan@setzler.de>
2022-04-30 13:00:40 +0200

    add alt prop

--- src/components/ImageTextCard.js
+++ src/components/ImageTextCard.js
@@ -1,6 +1,6 @@
 export default function ImageTextCard(props) {
     return <div className="imageTextCard">
-        <div className="imageTextCard-Image"><img src={props.imgSource} height={64} width={64} /></div>
+        <div className="imageTextCard-Image"><img alt="" src={props.imgSource} height={64} width={64} /></div>
         <div className="imageTextCard-Text"><p>{props.text}</p></div>
     </div>
 }
