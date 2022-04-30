ae1d77b054628b4d6a30cd383fb4ff9ce3a64815
Jonathan Setzler <jonathan@setzler.de>
2022-04-30 11:58:51 +0200

    make diceRollResult responsive

--- src/components/DiceRollResult.js
+++ src/components/DiceRollResult.js
@@ -1,5 +1,5 @@
 export default function DiceRollResult(props) {
-    return <div className="flexrow">
+    return <div className="flexrow diceSet">
         {props.rollResult.map((result, index) => <div className="die" key={index}>{result}</div>)}
     </div>
 }
