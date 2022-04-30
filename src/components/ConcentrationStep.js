93334e9b55de550bc1ae2a3c4624a09c84305d34
Jonathan Setzler <jonathan@setzler.de>
2022-04-30 11:58:23 +0200

    remove redundant information

--- src/components/ConcentrationStep.js
+++ src/components/ConcentrationStep.js
@@ -1,13 +1,15 @@
 import DiceRollResult from "./DiceRollResult";
 
 export default function ConcentrationStep(props) {
-    const style = props.total >= props.required ? {color: "green"} : {color: "red"}
+    const total = props.rollResult.reduce((a, b) => a + b)
+    const required = 10 + props.step*5
+    const style = total >= required ? {color: "green"} : {color: "red"}
     
     return (
         <div>
             <h4>Stufe {props.step}</h4>
             <DiceRollResult rollResult={props.rollResult} />
-            <p style={style}>Gesamt: {props.total}/{props.required}</p>
+            <p style={style}>Gesamt: {total}/{required}</p>
         </div>
     )
 }
