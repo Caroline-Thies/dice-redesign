8336b691631ef3d1d42ead924ad2ec5adac678a1
Jonathan Setzler <jonathan@setzler.de>
2022-04-30 13:01:39 +0200

    add alt prop

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,9 +12,9 @@ export default function SearchBar(props) {
     }
 
     return <div className="searchBar">
-        <img src={magnifying_glass} />
+        <img src={magnifying_glass} alt="magnifying glass" />
         <form>
             <input ref={ref} type="text" className="searchBarTextInput" placeholder="suche ..." onChange={event => props.setSearchText(event.target.value)} /> </form>
-        <img src={closeX} height={25} width={25} onClick={clearSearch}/>
+        <img src={closeX} height={25} width={25} onClick={clearSearch} alt="click here to clear search"/>
     </div>
 }
