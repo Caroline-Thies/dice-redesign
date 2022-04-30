8d2c125c8ce258d11a8640a1d301d5bfa38d1fc1
Jonathan Setzler <jonathan@setzler.de>
2022-04-30 09:15:19 +0200

    add filter and search to appselectview

--- src/views/AppSelectView.js
+++ src/views/AppSelectView.js
@@ -2,18 +2,31 @@ import filter from "../images/filter.png"
 
 import SearchBar from "../components/SearchBar"
 import AppCard from "../components/AppCard"
+import KraftgruppenPicker from "../components/KraftgruppenPicker"
+import { useState } from "react"
 
 export default function AppSelectView(props) {
-    
+    const [selectedKraftgruppen, setSelectedKraftgruppen] = useState([])
+    const [searchText, setSearchText] = useState("")
+
     const getApps = () => {
-        return props.knownApps
+        if (searchText === ""){
+            return props.knownApps.filter(app => selectedKraftgruppen.includes(app.kraftgruppe))
+        } else {
+            return props.knownApps.filter(app => selectedKraftgruppen.includes(app.kraftgruppe) &&
+                                        app.name.includes(searchText))
+        }
+        
     }
 
     return (
         <div className="flexcol">
-            <div className="flexrow">
-                <SearchBar />
+            <div className="flexrow sticky">
+                <SearchBar setSearchText={setSearchText} />
                 <img src={filter} width={35} height={35} />
+            </div>
+            <div>
+                <KraftgruppenPicker updateSelectedKraftgruppen={setSelectedKraftgruppen}/>   
             </div>
             <div className="appList">
                 {getApps().map(app => <AppCard name={app.name} kraftgruppe={app.kraftgruppe} key={app.name}/>)}
