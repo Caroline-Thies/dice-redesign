9f6b8d803444dd273f6af69a0f01e6a89aa39b14
Jonathan Setzler <jonathan@setzler.de>
2022-04-30 09:12:37 +0200

    expose searchString in SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,11 @@
 import magnifying_glass from "../images/magnifying_glass.svg"
 import closeX from "../images/closeX.png"
 
-export default function SearchBar() {
+export default function SearchBar(props) {
     return <div className="searchBar">
         <img src={magnifying_glass} />
         <form>
-            <input type="text" className="searchBarTextInput" placeholder="search ..." /> </form>
+            <input type="text" className="searchBarTextInput" placeholder="search ..." onChange={event => props.setSearchText(event.target.value)} /> </form>
         <img src={closeX} height={25} width={25}/>
     </div>
 }
