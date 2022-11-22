64408b04fb858e0c7cf36d91fc73cdbb6dec2932
carolinethies <itdesign@equeri.anonaddy.com>
2022-11-22 15:55:17 +0100

    replace backend calls with hardcoded data

--- src/BackendAdapter.js
+++ src/BackendAdapter.js
@@ -1,148 +1,235 @@
 export class BackendAdapter {
-  //static backendAddress =
-  //"https://dicecalculator-backend-sfvnbndcgv.azurewebsites.net";
-  static backendAddress = "http://localhost:5000";
-
   static async login(username, password) {
-    const user = { username: username, password: password };
-    const response = await (
-      await fetch(this.backendAddress + "/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify(user),
-      })
-    ).json();
-    return response;
+    return {
+      user: "testUser"
+    }
   }
 
   static async register(username, password) {
-    const user = { username: username, password: password };
-    alert("will attempt to register " + JSON.stringify(user));
-    const response = await (
-      await fetch(this.backendAddress + "/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify(user),
-      })
-    ).json();
-    alert("response: " + response);
-    return response;
+
   }
 
   static async logout() {
-    const response = await (
-      await fetch(this.backendAddress + "/logout", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-      })
-    ).json();
-    return response;
+
   }
 
   static async getAllCharacterNames() {
-    const response = await fetch(this.backendAddress + "/allCharacterNames", {
-      credentials: "include",
-    });
-    if (!response.ok) {
-      return [];
-    }
-    const data = await response.json();
-    return data;
+    return ["Alexander der Große", "Maximilian Mustermann"]
   }
 
   static async getCharacterByName(name) {
-    const response = await fetch(this.backendAddress + "/oneCharacter", {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ characterName: name }),
-    });
-    const char = await response.json();
-    if (!response.ok || !char.name) {
-      return undefined;
+    if (name === "Alexander der Große") {
+      return {
+        name: "Alexander der Große",
+        hasPsiModule: false,
+        wisdom: 5,
+        selfControl: 3,
+        kraftgruppen: {
+          Animakinese: 5,
+          Audiokinese: 0,
+          Biokinese: 0,
+          Chronokinese: 0,
+          Cryokinese: 0,
+          Elektrokinese: 0,
+          Momenkinese: 0,
+          Morphokinese: 0,
+          Photokinese: 0,
+          Psychokinese: 0,
+          Pyrokinese: 3,
+          Technokinese: 0,
+          Telekinese: 0
+        },
+        psiApps: {
+          "Animakinese": [
+            {
+              name: "Spiritwalk",
+              minValue: 11,
+              maxValue: 15,
+              noobLevel: 18,
+              masterLevel: 8,
+              kraftgruppe: "Animakinese",
+              description: "Der Wirker löst seine Neuralprojektion vom Körper und kann sich so 1+[KS] Spielrunden frei „Schnelligkeit x10 Einheiten“ schnell bewegen. Die Materie behindert die NP dabei nicht und er verfügt über sämtliche natürlichen Wahrnehmungen seines eigentlichen Körpers. Der Körper des Wirkers kann während dessen nicht benutzt werden. Nach ablauf der Zeit, wird die NP sofort in den Körper des Wirkers zurück geschleudert."
+            },
+            {
+              name: "Seelenwandel",
+              minValue: 23,
+              maxValue: 25,
+              noobLevel: 30,
+              masterLevel: undefined,
+              kraftgruppe: "Animakinese",
+              description: "Mit dem erfolgreichem wirken dieser App kann der Wirker Neuralprojektionen, die nicht in einem Körper stecken, die er aufgespürt hat (siehe App „Suche“ oder „Jenseitsbrücke“) oder in seinem Einflussbereich hält (siehe App „Kick“) in einen lebenden Körper transferieren. Je erfolgreichem Einsatz dieser App, verliert der Wirker 5-[KS] Psyche. Die Neuralprojektion kann nur in Körper eingefügt werden, die nicht bereits von einer NP belegt sind."
+            },
+            {
+              name: "Orakel",
+              minValue: 8,
+              maxValue: 12,
+              noobLevel: 14,
+              masterLevel: 6,
+              kraftgruppe: "Animakinese",
+              description: "Der Wirker kann die NP aller lebenden Organismen in einem Radius von 5+[KS]x10 Metern wahrnehmen. Für [4KS] kann der Wirker ALLES, farblos erkennen, was sich im Radius befindet."
+            }
+          ],
+          "Audiokinese": [],
+          "Biokinese": [],
+          "Chronokinese": [],
+          "Cryokinese": [],
+          "Elektrokinese": [],
+          "Heimakinese": [],
+          "Momenkinese": [],
+          "Morphokinese": [],
+          "Photokinese": [],
+          "Psychokinese": [],
+          "Pyrokinese": [
+            {
+              name: "Erhitzen",
+              minValue: 8,
+              maxValue: 12,
+              noobLevel: 14,
+              masterLevel: 6,
+              kraftgruppe: "Pyrokinese",
+              description: "Der Wirker ist in der Lage Objekte bis zu 100+[KS]x30°C und die Umgebung bis zu 20+[KS]x10°C zu erhitzen. Je nach Temperatur kommt es dazu das Gegenstände Feuer fangen."
+            },
+            {
+              name: "Lavafalle",
+              minValue: 11,
+              maxValue: 15,
+              noobLevel: 18,
+              masterLevel: 8,
+              kraftgruppe: "Pyrokinese",
+              description: ""
+            }
+          ],
+          "Technokinese": [],
+          "Telekinese": []
+        }
+      }
     }
-    return char;
+    else if (name === "Maximilian Mustermann") {
+      return {
+        name: "Alexander der Große",
+        hasPsiModule: false,
+        wisdom: 5,
+        selfControl: 6,
+        kraftgruppen: {
+          Animakinese: 3,
+          Audiokinese: 0,
+          Biokinese: 0,
+          Chronokinese: 0,
+          Cryokinese: 0,
+          Elektrokinese: 0,
+          Momenkinese: 0,
+          Morphokinese: 0,
+          Photokinese: 0,
+          Psychokinese: 0,
+          Pyrokinese: 5,
+          Technokinese: 0,
+          Telekinese: 0
+        },
+        psiApps: {
+          "Animakinese": [
+            {
+              name: "Spiritwalk",
+              minValue: 11,
+              maxValue: 15,
+              noobLevel: 18,
+              masterLevel: 8,
+              kraftgruppe: "Animakinese",
+              description: "Der Wirker löst seine Neuralprojektion vom Körper und kann sich so 1+[KS] Spielrunden frei „Schnelligkeit x10 Einheiten“ schnell bewegen. Die Materie behindert die NP dabei nicht und er verfügt über sämtliche natürlichen Wahrnehmungen seines eigentlichen Körpers. Der Körper des Wirkers kann während dessen nicht benutzt werden. Nach ablauf der Zeit, wird die NP sofort in den Körper des Wirkers zurück geschleudert."
+            },
+            {
+              name: "Seelenwandel",
+              minValue: 23,
+              maxValue: 25,
+              noobLevel: 30,
+              masterLevel: undefined,
+              kraftgruppe: "Animakinese",
+              description: "Mit dem erfolgreichem wirken dieser App kann der Wirker Neuralprojektionen, die nicht in einem Körper stecken, die er aufgespürt hat (siehe App „Suche“ oder „Jenseitsbrücke“) oder in seinem Einflussbereich hält (siehe App „Kick“) in einen lebenden Körper transferieren. Je erfolgreichem Einsatz dieser App, verliert der Wirker 5-[KS] Psyche. Die Neuralprojektion kann nur in Körper eingefügt werden, die nicht bereits von einer NP belegt sind."
+            },
+            {
+              name: "Orakel",
+              minValue: 8,
+              maxValue: 12,
+              noobLevel: 14,
+              masterLevel: 6,
+              kraftgruppe: "Animakinese",
+              description: "Der Wirker kann die NP aller lebenden Organismen in einem Radius von 5+[KS]x10 Metern wahrnehmen. Für [4KS] kann der Wirker ALLES, farblos erkennen, was sich im Radius befindet."
+            }
+          ],
+          "Audiokinese": [],
+          "Biokinese": [],
+          "Chronokinese": [],
+          "Cryokinese": [],
+          "Elektrokinese": [],
+          "Heimakinese": [],
+          "Momenkinese": [],
+          "Morphokinese": [],
+          "Photokinese": [],
+          "Psychokinese": [],
+          "Pyrokinese": [
+            {
+              name: "Erhitzen",
+              minValue: 8,
+              maxValue: 12,
+              noobLevel: 14,
+              masterLevel: 6,
+              kraftgruppe: "Pyrokinese",
+              description: "Der Wirker ist in der Lage Objekte bis zu 100+[KS]x30°C und die Umgebung bis zu 20+[KS]x10°C zu erhitzen. Je nach Temperatur kommt es dazu das Gegenstände Feuer fangen."
+            },
+            {
+              name: "Lavafalle",
+              minValue: 11,
+              maxValue: 15,
+              noobLevel: 18,
+              masterLevel: 8,
+              kraftgruppe: "Pyrokinese",
+              description: ""
+            }
+          ],
+          "Technokinese": [],
+          "Telekinese": []
+        }
+      }
+    }
   }
 
   static async addCharacter(character) {
-    character.psiApps = {
-      Animakinese: [],
-      Audiokinese: [],
-      Biokinese: [],
-      Chronokinese: [],
-      Cryokinese: [],
-      Elektrokinese: [],
-      Momenkinese: [],
-      Morphokinese: [],
-      Photokinese: [],
-      Psychokinese: [],
-      Pyrokinese: [],
-      Technokinese: [],
-      Telekinese: [],
-    };
-    const response = await (
-      await fetch(this.backendAddress + "/addCharacter", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(character),
-        credentials: "include",
-      })
-    ).json();
-    return response;
+
   }
 
   static async getAllAppNamesByKraftgruppe(kraftgruppe) {
-    const apps = await (
-      await fetch(
-        this.backendAddress +
-          "/getAppNames?" +
-          new URLSearchParams({ kraftgruppe: kraftgruppe }),
-        { credentials: "include" }
-      )
-    ).json();
-    return apps;
+    switch (kraftgruppe) {
+      case "Animakinese":
+        return ["Spiritwalk",
+          "Seelenwandel",
+          "Orakel",
+          "Good Guess",
+          "Suche",
+          "Remote Viewing",
+          "Jenseitsbrücke",
+          "Hellsicht",
+          "Kick",
+          "PSI-Sicht"]
+      case "Pyrokinese":
+        return ["Erhitzen",
+          "Lavafalle",
+          "Flammenbändiger",
+          "Inferno",
+          "Flammenhand",
+          "Explosion",
+          "Entzünden",
+          "Schmelzen",
+          "Absorption",
+          "Hitzeschock"]
+      default:
+        return []
+    }
   }
 
   static async setAppLearned(charName, appName, learned) {
-    const response = await (
-      await fetch(this.backendAddress + "/setAppLearned", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          characterName: charName,
-          appName: appName,
-          learned: learned,
-        }),
-        credentials: "include",
-      })
-    ).json();
+
   }
 
   static async updateCharacter(character) {
-    const response = await (
-      await fetch(this.backendAddress + "/updateCharacter", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(character),
-        credentials: "include",
-      })
-    ).json();
+
   }
 }
